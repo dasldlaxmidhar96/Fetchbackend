97010b648818e9079476de413602567b6fe57203
LAXMIDHAR DAS <laxmidhardas0000@gmail.com>
2022-09-22 12:52:29 +0530

    Fetchbackend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [backendData, setBackEndData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("http://localhost:3005/api").then(response => {
+      response.json().then((data)=>{
+        setBackEndData(data);
+      })
+    })
+  }, []);
+
+  return( 
+    <div>
+        {(typeof backendData.users === "undefined") ? (
+          <p>Loading...</p>
+        ): (
+          backendData.users.map((user,i)=>{
+          return <p key={i}>{user}</p>
+          })
+        )}
     </div>
   );
-}
+};
 
 export default App;
